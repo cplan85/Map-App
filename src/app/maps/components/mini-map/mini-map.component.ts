16d13e277846c3793bb8ee8ffc4b4103853e5fed
cplan85 <carlos_planchart@live.com>
2022-08-18 00:07:36 +0200

    finished mini map components

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; 
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.scss']
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('map') divMap!: ElementRef
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement, // container ID
+      //mapbox://styles/cplan203/cl6vfqkid005814p4gyy80dlc
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      projection: {name: 'globe' },// display the map as a 3D globe
+      interactive: false
+      });
+      map.on('style.load', () => {
+      map.setFog({}); // Set the default atmosphere style
+      });
+
+    new mapboxgl.Marker()
+    .setLngLat( this.lngLat)
+    .addTo(map)
+   
+    
   }
 
 }
